24227ee25008eaa3a032e6d4777fa4e051221559
Mohsin Mehmood <55545648+mohsinmahmood12@users.noreply.github.com>
2020-08-12 20:24:42 +0500

    Create returnbook.h

---
+++ returnbook.h
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+class return_book
+{
+private:
+	string rbid;
+	string rsid;
+	int Idcheck, rcheck;
+	string bid, n, nop, an, in, stdid;
+	int np = 0, t = 0, Uid, Urno;
+	int ID, Rno, noib;
+	string Bkname, Stdname, Aname, Iname, Dep, semster;
+	int n1 = 0, n2 = 0;
+public:
+	void ret_book();
+};
